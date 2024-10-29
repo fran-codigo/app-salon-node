91407e48eb65168f6916a879d955b67ccf525894
Franco <franco_tigres18@hotmail.com>
2024-10-28 21:35:16 -0600

    feat(HTML Email):Account Verify email with html and css

--- emails/authEmailServer.js
+++ emails/authEmailServer.js
@@ -1,6 +1,9 @@
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import { createTransport } from '../config/nodemailer.js';
 
-export async function sendEmailVerification({ name, email, token }) {
+async function sendEmail(templateName, subject, data, email = '') {
   const transporter = createTransport(
     process.env.EMAIL_HOST,
     process.env.EMAIL_PORT,
@@ -8,20 +11,40 @@ export async function sendEmailVerification({ name, email, token }) {
     process.env.EMAIL_PASS
   );
 
-  // Enviar el email
-  const info = await transporter.sendMail({
-    from: 'AppSalon <cuentas@appsalon.com>',
-    to: email,
-    subject: 'AppSalon - Confirma tu Cuenta',
-    text: 'AppSalon - Confirma tu Cuenta',
-    html: `<p>Hola ${name}, confirma tu cuenta en App Salon</p>
-    <p>Tu cuenta esta casi lista, solo debes confirmarla en el siguiente enlace</p>
-    <a href="${process.env.FRONTEND_URL}/auth/confirmar-cuenta/${token}">Confirmar cuenta</a>
-    <p>Si tu no creaste esta cuenta puedes ignorar este mensaje</p>
-    `,
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const templatePath = path.join(
+    __dirname,
+    '../emails/templates',
+    templateName
+  );
+  let html = readFileSync(templatePath, 'utf-8');
+
+  Object.entries(data).forEach(([key, value]) => {
+    html = html.replace(`{{${key}}}`, value);
   });
 
-  console.log('Mensaje enviado', info.messageId);
+  await transporter.sendMail({
+    from: 'AppSalon <cuentas@appsalon.com>',
+    to: email,
+    subject,
+    text: subject,
+    html,
+  });
+}
+
+export async function sendEmailVerification({ name, email, token }) {
+  sendEmail(
+    'emailVerification.html',
+    'Verifica tu cuenta en App salón',
+    {
+      name,
+      url: process.env.FRONTEND_URL,
+      token,
+    },
+    email
+  );
 }
 
 export async function sendEmailPassword({ name, email, token }) {
